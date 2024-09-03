b019546c0441069ba66ab69087e543cc37f5f391
gitadas <das_gita@hotmail.com>
2024-09-03 14:06:34 +1000

    one test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,44 @@
+// All test scripts are placed here in App.test.js. They can be placed in a separate
+// folder called "__test__" in components, and one file for each component. 
+// For ex. Navigation.test.js  
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Header from './components/Header/Header'; 
+import Navigation from './components/Navigation/Navigation'; 
+import Main from './components/Main/Main';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Creative Demonstrations
-  /i);
-  expect(linkElement).toBeInTheDocument();
+/* Test script 1 - checks for header text*/
+describe('Header', () => {
+  test('Header is rendered', () => {
+    render(<Header />);
+    const linkElement = screen.getByText("Creative Designs Bakery");
+   
+    // explicit assertion
+    expect(linkElement).toBeInTheDocument();
+  });
+});
+
+/* Test script 2 - checks for Navigation items */
+describe('Navigation', () => {
+  test('renders navigation items', () => {
+    const { getByText } = render(<Navigation />);
+    
+    // Verify that all navigation items are rendered
+    expect(getByText('Welcome')).toBeInTheDocument();
+    expect(getByText('Photo Gallery')).toBeInTheDocument();
+    expect(getByText('Weather Information')).toBeInTheDocument();
+    expect(getByText('Community Blog')).toBeInTheDocument();
+    expect(getByText('Contact Us')).toBeInTheDocument();
+  });  
+});
+
+/* Test script 3 - checks for flower image */
+describe('Main', () => {
+  test('renders image', () => {
+    const { getByText } = render(<Main />);
+    const sampleImg = screen.getByAltText('sampleImage');
+    expect(sampleImg).toBeInTheDocument();
+    expect(sampleImg.src).toContain('flower.jpg'); // Assuming 'flower.jpg' is the correct path
+  });  
 });
